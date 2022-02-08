ba14111066e8559f0f4fbecdccc80fa6d882d527
R. Ehmke <rehmke@users.noreply.github.com>
2022-02-08 17:31:47 -0600

    Update Config.java

--- src/edu/ucr/cs242/project/Config.java
+++ src/edu/ucr/cs242/project/Config.java
@@ -19,10 +19,10 @@ public class Config {
     public static final String UNSPLASH_API_USER_ENDPOINT = "https://api.unsplash.com" + "/users";
     public static final String UNSPLASH_API_ENDPOINT = UNSPLASH_API_RANDOM_ENDPOINT;  
     
-    public static final String UNSPLASH_ACCESS_KEY = EncDec.dec("hbb+zVyFlaOpaRpR3ZGWsT0E8UB9NhAcJdBiswmd8s+o7Ikl56ap3wJLlaOusaOtTQgjQvrw66w2w5LneSAexSnbNMS6wmf4T4Qycv2HT/U=");
+    public static final String UNSPLASH_ACCESS_KEY = EncDec.dec("ENCRYPTED_UNSPLASH_ACCESS_KEY_HERE");
     public static final String UNSPLASH_SECRET_KEY = null;
     
-    public static final String ARCHIVE_FILEPATH_ROOT = "/Users/rehmke/development/school/Cs242/DATA";
+    public static final String ARCHIVE_FILEPATH_ROOT = "/TMP/CS242/PROJECT/OUTPUT";
     public static final String METADATA_ARCHIVE_FILEPATH = ARCHIVE_FILEPATH_ROOT + System.getProperty("file.separator") + "METADATA";
     public static final String IMAGE_ARCHIVE_FILEPATH = ARCHIVE_FILEPATH_ROOT + System.getProperty("file.separator") + "IMAGE";
     public static final String INDEXABLE_ARCHIVE_FILEPATH = ARCHIVE_FILEPATH_ROOT + System.getProperty("file.separator") + "INDEXABLE";
