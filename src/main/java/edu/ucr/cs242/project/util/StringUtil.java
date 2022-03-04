c96b3cdeb53bc0e952c9c50ab45e33c94cfa3374
R. Ehmke <rehmke@users.noreply.github.com>
2022-03-03 21:47:07 -0600

    Add files via upload

--- src/main/java/edu/ucr/cs242/project/util/StringUtil.java
+++ src/main/java/edu/ucr/cs242/project/util/StringUtil.java
@@ -1,5 +1,7 @@
 package edu.ucr.cs242.project.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  *
  * @author rehmke
@@ -8,6 +10,19 @@ public class StringUtil {
  
     public static void announce(String _val) {
         System.out.println(_val);
+    }    
+    
+    public static String scrub(String _val) {
+        String retVal = "";
+        retVal = _val;
+        retVal = StringUtils.replace(retVal, "_", "");
+        retVal = StringUtils.replace(retVal, "\\r\\n", "");
+        retVal = StringUtils.replace(retVal, "\r\n", "");
+        retVal = StringUtils.replace(retVal, "\n", "");
+        retVal = StringUtils.replace(retVal, "\\n", "");
+        retVal = StringUtils.replace(retVal, "\\u0027", "");
+        retVal = StringUtils.replace(retVal, "\u0027", "");
+        return retVal;
     }    
     
 }
