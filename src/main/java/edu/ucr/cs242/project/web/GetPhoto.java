b372019d1b1a12ce5b89163a1706ed7ca33a05cf
R. Ehmke <rehmke@users.noreply.github.com>
2022-03-07 22:29:00 -0600

    Add files via upload

---
+++ src/main/java/edu/ucr/cs242/project/web/GetPhoto.java
@@ -0,0 +1,43 @@
+package edu.ucr.cs242.project.web;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author rehmke
+ */
+public class GetPhoto extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        // serve logo
+        String filepath = "/Users/rehmke/development/school/Cs242/DATA/IMAGE/" + req.getParameter("id");
+        File file = new File(filepath);
+        res.reset();
+        res.setBufferSize(10240);
+        res.setContentLength((int) file.length());
+        ServletOutputStream fout = res.getOutputStream();
+        res.setHeader("Content-Type", "image/png");
+        res.setHeader("Content-Length", String.valueOf(file.length()));
+        res.setHeader("Content-Disposition", "inline;filename=" + req.getParameter("id") + ".png");
+
+        int length = 0;
+        byte[] bbuf = new byte[100];
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        while ((in != null) && ((length = in.read(bbuf)) != -1)) {
+            fout.write(bbuf, 0, length);
+        }
+        in.close();
+        fout.flush();
+        fout.close();
+
+    }
+
+}
